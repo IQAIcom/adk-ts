9b5fde2a6c6982f7b688e04b0411e840c66f186b
Marvel.Codes <marvellousn561@gmail.com>
2025-11-21 23:38:13 +0100

    setup

---
+++ apps/docs/app/showcase/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Hero } from "./_components/hero";
+
+const ShowcasePage = () => {
+	return (
+		<div className="flex flex-col min-h-screen w-screen">
+			<Hero />
+			<h1>Showcase</h1>
+		</div>
+	);
+};
+
+export default ShowcasePage;
